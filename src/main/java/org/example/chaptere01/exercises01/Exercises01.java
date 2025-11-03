3fe1c01e1ce0c10a9145dffd632388fad027a561
Saeid Zorofchi <saeid.zorofchi@gmail.com>
2025-11-03 22:02:23 +0330

    chapter 01 - Exercises01 & 02

---
+++ src/main/java/org/example/chaptere01/exercises01/Exercises01.java
@@ -0,0 +1,11 @@
+package org.example.chaptere01.exercises01;
+
+
+
+public class Exercises01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println(" Programming is fun");
+    }
+}
---
+++ src/main/java/org/example/chaptere01/exercises02/Exercises02.java
@@ -0,0 +1,11 @@
+package org.example.chaptere01.exercises02;
+
+public class Exercises02 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
