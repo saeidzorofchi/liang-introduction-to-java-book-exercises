87f62f770371ff8646740e6e09db719b3866fb05
Saeid Zorofchi <saeid.zorofchi@gmail.com>
2025-11-20 23:40:49 +0330

    update 1

--- src/main/java/org/example/chapter02/exercise03/Exercise03.java
+++ src/main/java/org/example/chapter02/exercise03/Exercise03.java
@@ -9,6 +9,8 @@ public class Exercise03 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number in feet : ");
+
 
 
     }
